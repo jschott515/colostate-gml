0a50ca76dc802d8c2181e490ea5c683934e92410
jsch <jschott515@gmail.com>
2025-12-07 16:00:05 -0600

    Fix fib.c testcase

--- testcases/fib.c
+++ testcases/fib.c
@@ -3,17 +3,19 @@
 #include <omp.h>
 
 long long fib(int n) {
-    if (n <= 1) return 1;
+    long long result, a, b;
 
-    long long a, b;
+    if (n <= 1) result = 1;
+    else
+    {
+        #pragma omp task shared(a) if(n > 20)
+        a = fib(n - 1);
+        b = fib(n - 2);
+        #pragma omp taskwait
 
-    #pragma omp task shared(a) if(n > 20)
-    a = fib(n - 1);
-
-    b = fib(n - 2);
-
-    #pragma omp taskwait
-    return a + b;
+        result = a + b;
+    }
+    return result;
 }
 
 int main(int argc, char** argv) {
